61af0f1d1f572e45a0736f7d536adafd276ba087
Anna Deeva <ann.deeva@gmail.com>
2023-02-19 19:50:22 +0100

    Don't reload after deletion

--- static/js/rfid.js
+++ static/js/rfid.js
@@ -17,22 +17,21 @@ function onCheckboxChange(context) {
 	xhr.send(data);
 }
 
-function onDeleteClick(id_value) {
+function onDeleteClick(id_value, a) {
 	var data = new FormData();
 	data.append('operation', 'delete');
 	data.append('id', id_value);
 	data.append('device', '');
 	data.append('state', '');
 
-	console.log(id_value);
 	var xhr = new XMLHttpRequest();
+	var table = document.getElementById('tblData');
+	var rowIndex = a.parentNode.parentNode.rowIndex;
+	table.deleteRow(rowIndex);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4) {
 			if (xhr.status != 200) {
 				alert("Cannot get updated table!");
-			} else {
-				// Reload page when we received response
-				location.reload(true);
 			}
 		}
 	}
