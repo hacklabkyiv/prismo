fb3fa3c2560902484ad2afc88254d12bd32adb32
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2023-10-14 15:03:49 +0300

    Clean code

--- users_view_model.py
+++ users_view_model.py
@@ -3,7 +3,8 @@ from typing import List
 
 from app.data.device_repository import get_all_devices
 from app.data.permissions_repository import get_user_permissions
-from app.data.user_repository import get_all_users, UserWithLastEnter
+from app.data.user_dto import UserDto
+from app.data.user_repository import get_all_users
 
 
 @dataclass()
@@ -20,13 +21,11 @@ class PermissionUiModel:
 class AccessControlPanelRow:
     user_name: str
     user_key: str
-    last_enter: str
     permissions: List[PermissionUiModel]
 
-    def __init__(self, user_name, user_key, last_enter, permissions):
+    def __init__(self, user_name, user_key, permissions):
         self.user_name = user_name
         self.user_key = user_key
-        self.last_enter = last_enter
         self.permissions = permissions
 
 
@@ -44,7 +43,7 @@ def get_access_control_panel() -> AccessControlPanel:
     users = get_all_users()
     devices = get_all_devices()
 
-    header = ['User name', 'User key', 'Last enter']
+    header = ['User name', 'User key']
     for device in devices:
         header.append(device.name)
 
@@ -57,12 +56,11 @@ def get_access_control_panel() -> AccessControlPanel:
     return AccessControlPanel(header, rows)
 
 
-def build_user_access_control_permissions_row(user: UserWithLastEnter, devices):
-    grated_user_device = get_user_permissions(user.user.key)
+def build_user_access_control_permissions_row(user: UserDto, devices):
+    grated_user_device = get_user_permissions(user.key)
     user_permissions_ui_models = []
     for device in devices:
         permission_model = PermissionUiModel(device.id in grated_user_device, device.id)
         user_permissions_ui_models.append(permission_model)
 
-    return AccessControlPanelRow(user.user.name, user.user.key, user.last_enter,
-                                 user_permissions_ui_models)
+    return AccessControlPanelRow(user.name, user.key, user_permissions_ui_models)
